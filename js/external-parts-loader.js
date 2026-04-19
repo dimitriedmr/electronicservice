11f3ad443f85404cde767901e7e52a015d8bb145
dimitriedmr <panainte.dimitrie@gmail.com>
2026-04-20 01:51:30 +0300

    3 images are loaded into carousel slides

--- js/external-parts-loader.js
+++ js/external-parts-loader.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             document.getElementById("nav-placeholder").innerHTML = data;
         })
-        .catch(error => console.error('Error loading the navigation:', error));
+        .catch(error => console.error('Error loading navigation:', error));
 
     fetch("parts/footer.html")
         .then(response2 => response2.text())
         .then(data2 => {
             document.getElementById("footer-placeholder").innerHTML = data2;
         })
-        .catch(error2 => console.error('Error loading the navigation:', error2));
+        .catch(error2 => console.error('Error loading footer:', error2));
 });
