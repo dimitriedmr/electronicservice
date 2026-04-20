851f1fe3a60251e424fd7dfe558bf5c7a3f4f7f3
dimitriedmr <panainte.dimitrie@gmail.com>
2026-04-20 11:29:53 +0300

    maps location added

--- js/external-parts-loader.js
+++ js/external-parts-loader.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("parts/nav.html")
+    fetch("nav.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("nav-placeholder").innerHTML = data;
         })
         .catch(error => console.error('Error loading navigation:', error));
 
-    fetch("parts/footer.html")
+    fetch("footer.html")
         .then(response2 => response2.text())
         .then(data2 => {
             document.getElementById("footer-placeholder").innerHTML = data2;
